d3dc1d12ea8ea0de71e9f8b847d80d33792651a4
TomWesterhout <westerhout_tom@hotmail.com>
2017-02-24 15:27:33 +0100

    Added email uniqueness and format validations to the User model, including databse uniqueness.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,4 +30,37 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
+	test "email validation should accept valid addressess" do
+		valid_addressess = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
+			first.last@foo.jp alice+bob@baz.cn]
+
+		valid_addressess.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
+
+	test "email validation should reject invalid addressess" do
+		invalid_addressess = %w[user@example,com user_at_foo.org user.name@example. 
+			foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+		invalid_addressess.each do |invalid_address|
+			@user.email = invalid_address
+			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+		end
+	end
+
+	test "email addresses should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+	test "email addresses should be saved as lower-case" do
+		mixed_case_email = "Foo@ExAMPle.CoM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
 end
