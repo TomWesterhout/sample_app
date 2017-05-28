b54cd50501f8c464c706313f8e29c9a8d1615e72
TomWesterhout <westerhout_tom@hotmail.com>
2017-05-28 15:19:08 +0200

    Added update action to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,27 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params) # Not the final implementation!
-  	if @user.save
+  	@user = User.new(user_params)
+    if @user.save
       log_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
-  		render :new
+  		render 'new'
   	end
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 
   private
