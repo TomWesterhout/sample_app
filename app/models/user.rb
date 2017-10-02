24f5c1cc654f8930d537efe54ee07f74f1213513
TomWesterhout <westerhout_tom@hotmail.com>
2017-10-02 21:33:05 +0200

    Added microposts integration test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
 	# Returns the hash digest of the given string.
 	def self.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-
 		BCrypt::Password.create(string, cost: cost)
 	end
 
