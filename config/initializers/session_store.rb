1d078f7d9339ee27ba53bc2fbcf7a1377a157b0d
TomWesterhout <westerhout_tom@hotmail.com>
2017-10-04 17:06:39 +0200

    Added image resizing on upload.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+if Rails.env.test?
+	CarrierWave.configure do |config|
+		config.enable_processing = false
+	end
+end
