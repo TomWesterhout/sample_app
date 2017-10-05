46c698a8f53fbea4f0241349c22e128eda0c9a5d
TomWesterhout <westerhout_tom@hotmail.com>
2017-10-05 13:28:43 +0200

    minor changes to carrierwave file.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
 		config.fog_provider = 'fog/aws'
 		config.fog_credentials = { 
 			# Configuration for Amazon S3
-			provider:								'AWS',
-			aws_access_key_id: 			ENV['AKIAJVGPPQMJD3XDCNBQ'],
-			aws_secret_access_key: 	ENV['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'],
-			region: 								'eu-central-1'
+			:provider								=> 'AWS',
+			:aws_access_key_id 			=> ENV['AKIAJVGPPQMJD3XDCNBQ'],
+			:aws_secret_access_key 	=> ENV['xyalzt91CFe3zWTqvwKdPIqQagV+OVO9rJk+Z3Mg'],
+			:region 								=> 'eu-central-1'
 		}
 		config.fog_directory = ENV['sampleapptwidder']
 	end
