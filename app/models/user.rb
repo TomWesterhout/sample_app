25fdd922aa796dba3e4a06ca10ec5a972b6e3a58
TomWesterhout <westerhout_tom@hotmail.com>
2017-10-30 21:44:23 +0100

    Refactored User model feed method to include al following ids and the user id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,7 +71,7 @@ class User < ApplicationRecord
 	# Defines a proto-feed.
 	# See "Following user" for the full implementation
 	def feed
-		Micropost.where("user_id=?", id)
+		Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
 	end
 
 	# Follows a user.
